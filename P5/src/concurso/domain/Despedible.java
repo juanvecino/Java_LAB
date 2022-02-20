4921ef86fff19207d1d248aa758983ad67d386fa
“juanvecino” <49287699+juanvecino@users.noreply.github.com>
2022-02-20 21:14:45 +0100

    Eliminación de cosas pero luego es lo mismo

--- P5/src/concurso/domain/Despedible.java
+++
@@ -1,8 +0,0 @@
-package concurso.domain;
-
-public interface Despedible{
-  //No hace falta poner abstract, porque todos los métodos de un interface son abstractos
-  public abstract float calcularIndemnizacionDespido(float salario, int diasContrato);
-  public String calcularUltimoDia(int diasContrato);
-  
-}
